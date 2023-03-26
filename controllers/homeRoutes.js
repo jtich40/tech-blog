95c54db820a453ba3bdb1e7324d6dc48d844ba31
Jared Tichacek <jared.tichacek@gmail.com>
2023-03-26 16:33:30 -0500

    fixed dashboard route to show all blogs and fixed homepage handlebars to display comments on single post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -44,22 +44,22 @@ router.get('/login', async (req ,res) => {
 })
 
 // get dashboard page, use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req ,res) => {
-    try {
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: { exclude: ['password'] },
-            include: [{ model: BlogPost }]
-            })
-        const user = userData.get({ plain: true })
-        res.render('dashboard', {
-            ...user,
-            logged_in: true
-        })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
-})
+// router.get('/dashboard', withAuth, async (req ,res) => {
+//     try {
+//         const userData = await User.findByPk(req.session.user_id, {
+//             attributes: { exclude: ['password'] },
+//             include: [{ model: BlogPost }]
+//             })
+//         const user = userData.get({ plain: true })
+//         res.render('dashboard', {
+//             ...user,
+//             logged_in: true
+//         })
+//     }
+//     catch (err) {
+//         console.log(err)
+//         res.status(500).json(err)
+//     }
+// })
 
 module.exports = router
